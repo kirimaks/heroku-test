cfa32bc68e2d4cc32468f59459415f6f098002c2
kirimaks <kirimaks@yahoo.com>
2016-06-21 22:16:03 +0300

    sqlite3

--- routes.py
+++ routes.py
@@ -1,8 +1,14 @@
 from flask import Flask
+import sqlite3
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
+    db = sqlite3.connect("mydb")
+    cursor = db.cursor()
+
     return "Hello World!"
 
 if __name__ == "__main__":
