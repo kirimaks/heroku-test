8609aabff7b4c6be883b9e4628e9b105a93372ad
kirimaks <kirimaks@yahoo.com>
2016-06-22 14:10:13 +0300

    proxy_list

--- routes.py
+++ routes.py
@@ -1,10 +1,9 @@
-from flask import Flask
-from db_test import db_test
-# from random import randrange
+from flask import Flask, Response
 from scheduler import Scheduler
+from proxy_list.get_proxy import GetProxy
+import json
 
 app = Flask(__name__)
-my_db = db_test()
 
 scheduler = Scheduler()
 scheduler.start()
@@ -12,8 +11,13 @@ scheduler.start()
 
 @app.route("/")
 def hello():
-    out = my_db.num
-    return str(out)
+    pl = GetProxy()
+
+    buff = [pl.get_proxy() for i in range(10)]
+
+    resp = Response(json.dumps(buff), status=200, mimetype="application/json")
+
+    return resp
 
 if __name__ == "__main__":
     app.run()
--- scheduler.py
+++ scheduler.py
@@ -1,28 +1,13 @@
 import threading
 import time
-from random import randrange
-from ConfigParser import ConfigParser
-import sqlite3
+import os
 
 
 class Scheduler(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        config = ConfigParser()
-        config.read("config.cfg")
-        self.db_file = config.get("Database", "file")
 
     def run(self):
         while True:
-            conn = sqlite3.connect(self.db_file)
-            with conn:
-                cursor = conn.cursor()
-                query = """
-                    INSERT INTO data(number) VALUES(?)
-                """
-
-                rnum = randrange(0, 1000)
-
-                cursor.execute(query, (rnum,))
-
-            time.sleep(10)
+            os.system("python proxy_list/create_proxy_list.py")
+            time.sleep(100)
