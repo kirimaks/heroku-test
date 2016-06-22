996036db428d9ab22d6b54468851b1e77ebdd34e
kirimaks <kirimaks@yahoo.com>
2016-06-22 15:42:11 +0300

    json charset

--- routes.py
+++ routes.py
@@ -11,13 +11,19 @@ scheduler.start()
 
 @app.route("/")
 def hello():
+    return "Hello world"
+
+
+@app.route("/proxy_list")
+def proxy_list():
     pl = GetProxy()
-
     buff = [pl.get_proxy() for i in range(10)]
-
-    resp = Response(json.dumps(buff), status=200, mimetype="application/json")
+    resp = Response(json.dumps(buff),
+                    status=200,
+                    content_type='application/json; charset=utf-8')
 
     return resp
+
 
 if __name__ == "__main__":
     app.run()
